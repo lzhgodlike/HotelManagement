7128d258b10f96ccc049cc449980a023e2fa5594
1816279799@qq.com <1816279799@qq.com>
2025-01-08 14:27:03 +0800

    修改现有房间的显示状态 删除FileUploadServlet中没用的方法

--- src/main/java/servlet/FileUploadServlet.java
+++ src/main/java/servlet/FileUploadServlet.java
@@ -3,7 +3,9 @@ package servlet;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Date;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.UUID;
 
@@ -21,6 +23,7 @@ import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import com.google.gson.Gson;
 
 import bean.ReturnBean;
+import util.DBConnection;
 
 @WebServlet("/fileUpload")
 public class FileUploadServlet extends HttpServlet {
@@ -49,7 +52,7 @@ public class FileUploadServlet extends HttpServlet {
                         //如果fileItem不是普通的表单元素，就可以将该元素写到后端数据库
                         if (!fileItem.isFormField()) {
                             //获取项目存储的磁盘地址,到项目文件夹为止
-                            String realPath = getServletContext().getRealPath("/") + "upload/";
+                            String realPath = getServletContext().getRealPath("/") + "/upload/";
                             //确保upload文件夹存在
                             File uploadDir = new File(realPath);
                             if (!uploadDir.exists()) {
@@ -60,7 +63,7 @@ public class FileUploadServlet extends HttpServlet {
                             //生成唯一的文件名
                             String uniqueFileName = UUID.randomUUID().toString() + name.substring(name.lastIndexOf("."));
                             //新建的文件相对项目文件夹的存储路径（项目文件夹下的upload文件夹）
-                            String filePath = "upload/" + uniqueFileName;
+                            String filePath = "/upload/" + uniqueFileName;
                             //新建文件，提供文件的绝对路径参数
                             File file = new File(realPath + uniqueFileName);
                             //将上传的文件复制给新建的file文件，write方法会自动关闭流。这个write方法是diskfileItem中的方法，自带关闭功能
@@ -69,9 +72,6 @@ public class FileUploadServlet extends HttpServlet {
                             returnBean.setCode(200);
                             returnBean.setMessage(filePath);
 
-                            // 将文件路径存储到数据库中的room表的room_img属性
-                            // 这里假设你有一个方法 saveRoomImage(String imagePath) 来保存图片路径到数据库
-                            saveRoomImage(filePath);
                         }
                     }
                 }
@@ -85,24 +85,5 @@ public class FileUploadServlet extends HttpServlet {
         String s = gson.toJson(returnBean);
         PrintWriter out = response.getWriter();
         out.write(s);
-    }
-
-    private void saveRoomImage(String imagePath) {
-        // 实现将图片路径保存到数据库中的逻辑
-        // 例如：
-        // Connection conn = null;
-        // PreparedStatement ps = null;
-        // try {
-        //     conn = DBConnection.getConnection();
-        //     String sql = "UPDATE room SET room_img = ? WHERE room_id = ?";
-        //     ps = conn.prepareStatement(sql);
-        //     ps.setString(1, imagePath);
-        //     ps.setInt(2, roomId); // 假设你有roomId
-        //     ps.executeUpdate();
-        // } catch (SQLException e) {
-        //     e.printStackTrace();
-        // } finally {
-        //     DBConnection.close(conn, ps, null);
-        // }
     }
 }
